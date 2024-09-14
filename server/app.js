541240f8c73aed85fc55f73d7c7d28653a23aa2c
vbvivek28 <vb79798@gmail.com>
2024-09-14 20:07:43 +0530

    minor change

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 app.use(cors({
-    origin: 'http://127.0.0.1:63317'
+    origin: '*',
   }));
 
 const pool = new Pool({
